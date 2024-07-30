3c43b3363462170ad1662d3c6ec60a6b9abb14f4
randyaf <randyaf29@gmail.com>
2024-07-30 14:25:58 +0700

    Add GameBoard module which contains main logic for the game
    these logic are including:
    - board state
    - check move validity
    - check winner

---
+++ script.js
@@ -0,0 +1,60 @@
+const GameBoard = (function() {
+    // const boardState = [];
+    const boardState = [1, "a-1", 2, "c-3", 3, "b-2"];
+
+    function tickCell(row, column) {
+        if (!checkMoveValidity(row, column)) return;
+        boardState.push(`${row}-${column}`);
+    }
+
+    function checkMoveValidity(row, column) {
+        if (boardState.find(coordinate => coordinate === `${row}-${column}`)) return false;
+        else if (!["a", "b", "c"].includes(row)) return false;
+        else if (column < 1 && column > 3) return false;
+        return true;
+    }
+
+    function checkWinner() {
+        if (boardState.length % 2 === 0) {
+            const evenPlayer = boardState.filter(coordinate => boardState.indexOf(coordinate) % 2 !== 0);
+            return findWinningLine(evenPlayer);
+        } else {
+            const oddPlayer = boardState.filter(coordinate => boardState.indexOf(coordinate) % 2 === 0);
+            return findWinningLine(oddPlayer);
+        }
+    }
+
+    function findWinningLine(playerCells) {
+        for (let row of ["a", "b", "c"]) {
+            const winningLine = [];
+            for (let column = 1; column <= 3; column++) {
+                if (!playerCells.includes(`${row}-${column}`)) break;
+                winningLine.push(`${row}-${column}`);
+                if (column === 3) return winningLine;
+            }
+            winningLine.splice(0, winningLine.length);
+        }
+        for (let column = 1; column <= 3; column++) {
+            const winningLine = [];
+            for (let row of ["a", "b", "c"]) {
+                if (!playerCells.includes(`${row}-${column}`)) break;
+                winningLine.push(`${row}-${column}`);
+                if (row === "c") return winningLine;
+            }
+            winningLine.splice(0, winningLine.length);
+        }
+        const diagonalRight = ["a-1", "b-2", "c-3"];
+        for (let coordinate of diagonalRight) {
+            if (!playerCells.includes(coordinate)) break;
+            if (coordinate === "c-3") return diagonalRight;
+        }
+        const diagonalLeft = ["a-3", "b-2", "c-1"];
+        for (let coordinate of diagonalLeft) {
+            if (!playerCells.includes(coordinate)) break;
+            if (coordinate === "c-1") return diagonalLeft;
+        }
+        return null;
+    }
+
+    return { boardState, tickCell, checkWinner };
+})();
